31f8b5ba1c2b4a8855b11dc28353fbb2f540075d
zoranaism <lazareviczorana@gmail.com>
2020-03-23 16:30:14 +0100

    Initial setup done, Created HomePages page

---
+++ src/pages/HomePagesList/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { Jumbotron } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+
+export default function HomePagesList() {
+  return (
+    <div>
+
+    <Jumbotron>
+    <h1>Homepages</h1>
+  </Jumbotron>
+  <Container>
+    <h2>Homepages cards</h2>
+  </Container>
+    </div>
+  )
+}
